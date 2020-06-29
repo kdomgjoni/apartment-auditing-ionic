5587b4289f830fd1424ed5772fdfddc1257df3e1
unknown <domgjonikrist@gmail.com>
2020-06-29 23:46:23 +0200

    setting up Firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,13 +9,28 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireDatabaseModule } from '@angular/fire/database';
+
+const config = {
+  apiKey: "AIzaSyCvhiBpfjIB8nkZjAxb7WYmg8FuoeJhL9I",
+  authDomain: "ionic-apartment-auditing.firebaseapp.com",
+  databaseURL: "https://ionic-apartment-auditing.firebaseio.com",
+  projectId: "ionic-apartment-auditing",
+  storageBucket: "ionic-apartment-auditing.appspot.com",
+  messagingSenderId: "165790949527",
+  appId: "1:165790949527:web:6f32549abfccf8e64049d2"
+};
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
   imports: [
     BrowserModule,
     IonicModule.forRoot(),
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(config),
+    AngularFireDatabaseModule
   ],
   providers: [
     StatusBar,
